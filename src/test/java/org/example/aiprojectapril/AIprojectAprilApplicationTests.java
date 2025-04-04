f511f3495306b632875a5fac1867a3ee461931b8
Nunsen <nuso0001@stud.kea.dk>
2025-04-04 10:45:17 +0200

    API key environment variable

---
+++ src/test/java/org/example/aiprojectapril/AIprojectAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aiprojectapril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIprojectAprilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
